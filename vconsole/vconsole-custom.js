b89d08e3bb8f03d96c08536276fde1e931d7ccbc
OxygenFox <andrysetyoso@gmail.com>
2020-04-30 21:02:10 +0700

    Add files via upload

---
+++ vconsole/vconsole-custom.js
@@ -0,0 +1,16 @@
+ $(document).ready(function() {
+	window.vConsole = new window.VConsole({
+    defaultPlugins: ['system',
+      'network',
+      'element',
+      'storage'],
+	  
+    maxLogNumber: 1000,
+    // disableLogScrolling: true,
+    onReady: function() {
+      console.log('vConsole is ready.');
+    },
+    onClearLog: function() {
+      console.log('on clearLog');
+    }
+  });
